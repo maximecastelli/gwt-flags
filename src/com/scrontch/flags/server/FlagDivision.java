78e2c5c5523cd8dd02188f2ff2ec41364c221032
lars.ruoff <lars.ruoff@gmail.com>
2011-05-30 20:52:53 +0000

    For v2

--- src/com/scrontch/flags/server/FlagDivision.java
+++ src/com/scrontch/flags/server/FlagDivision.java
@@ -4,10 +4,12 @@ package com.scrontch.flags.server;
 public class FlagDivision {
 	String name;
 	String svg;  //SVG source
+	int usedColors;
 
 	//Constructor
-	FlagDivision(String name, String svg) {
+	FlagDivision(String name, String svg, int usedColors) {
 		this.name = name;
 		this.svg = svg;
+		this.usedColors = usedColors;
 	}
 }
