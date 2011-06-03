f1b08195e6f1dfce1d89157714cdb5d26fa5f29e
lars.ruoff@gmail.com <lars.ruoff@gmail.com@2268dab5-5d08-7eea-80c2-3754516ff678>
2011-06-03 21:00:07 +0000

    Started work on interactive version.

--- trunk/src/com/scrontch/flags/client/GwtFlags.java
+++ trunk/src/com/scrontch/flags/client/GwtFlags.java
@@ -1,20 +1,24 @@
 package com.scrontch.flags.client;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.ToggleButton;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.SimplePanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -40,17 +44,105 @@ public class GwtFlags implements EntryPoint {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
+		FlagService.FlagInfo flagInfo = new FlagService.FlagInfo();
+		
 		final Button sendButton = new Button("Generate New Flag");
 
 		// We can add style names to widgets
 		//sendButton.addStyleName("sendButton");
 
+		FlagResources resources = GWT.create(FlagResources.class);
+		final String[] division_image_urls = new String[] {
+			"images/flag-div0.png",
+			"images/flag-div0.png",
+			"images/flag-div0.png",
+			"images/flag-div0.png",
+			"images/flag-div0.png",
+			"images/flag-div0.png",
+			"images/flag-div0.png",
+			"images/flag-div0.png",
+			"images/flag-div0.png",
+			"images/flag-div0.png",
+			"images/flag-div0.png"
+		};
+		
+		final String[] color_codes = new String[] {
+		        "#000000",
+		        "#ffffff",
+		        "#ce1126",
+		        "#00335b",
+		        "#006b3f",
+		        "#fcd116",
+		        "#ff8400",
+		        "#3a75c4"
+			};
+			
 		// Add the nameField and sendButton to the RootPanel
 		// Use RootPanel.get() to get the entire body element
-		RootPanel.get("sendButtonContainer").add(sendButton);
+		RootPanel rootPanel = RootPanel.get("sendButtonContainer");
+		rootPanel.add(sendButton);
 		RootPanel.get("flagContainer").add(flagWidget);
 		RootPanel.get("flagLink").add(flagLink);
 		
+		VerticalPanel verticalPanel = new VerticalPanel();
+		rootPanel.add(verticalPanel, 22, 66);
+		verticalPanel.setSize("100px", "100px");
+
+		final List<ToggleButton> divisionButtons = new ArrayList<ToggleButton>();
+		
+		class DivisionRadio implements ClickHandler {
+			ToggleButton button;
+			public DivisionRadio(ToggleButton button) {
+				this.button = button;
+				divisionButtons.add(button);
+			}
+			public void onClick(ClickEvent event) {
+				int i = divisionButtons.indexOf(button);
+				//TODO
+				
+				//iterator loop
+				Iterator<ToggleButton> iterator = divisionButtons.iterator();
+				while ( iterator.hasNext() ){
+					iterator.next().setDown(false);
+				}
+				button.setDown(true);
+			}
+		}
+
+		// divisions
+		{
+			Grid grid = new Grid((division_image_urls.length / 6)+1, 6);
+			//grid.setSize("100px", "100px");
+			verticalPanel.add(grid);
+		
+			for (int i=0; i<division_image_urls.length; i++) {
+				Image image = new Image(division_image_urls[i]);
+				image.setSize("36px", "24px");
+				ToggleButton button = new ToggleButton(image);
+				button.addStyleName("flag-icon");
+				grid.setWidget(i/6, i%6, button);
+				DivisionRadio radio = new DivisionRadio(button); 
+				button.addClickHandler(radio);
+			}
+		}
+	
+		
+		// division-colors 1
+		{
+			Grid grid = new Grid(1, color_codes.length);
+			//grid.setSize("100px", "100px");
+			verticalPanel.add(grid);
+		
+			for (int i=0; i<color_codes.length; i++) {
+				SimplePanel simplePanel = new SimplePanel();
+				simplePanel.addStyleName("color-icon");
+				grid.setWidget(0, i, simplePanel);
+				simplePanel.getElement().getStyle().setProperty("backgroundColor", color_codes[i]);
+//				simplePanel.addClickHandler(colorClickHandler);
+
+			}
+		}
+	
 		// Create the popup dialog box
 		final DialogBox dialogBox = new DialogBox();
 		dialogBox.setText("Remote Procedure Call");
